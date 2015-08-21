13a1a769067a3d29743533995e48512bc1a69498
John Ramsden <johnramsden@users.noreply.github.com>
2015-08-20 23:24:02 -0700

    Added new icons code.

--- QRaspbed/Raspbed.cpp
+++ QRaspbed/Raspbed.cpp
@@ -6,7 +6,7 @@
 Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), bed() {
     ui->setupUi(this);
 
-    QPixmap headUpPixmap("images/head-up-icon.jpg");
+    QPixmap headUpPixmap("images/head-up-icon.png");
     QIcon headUpButtonIcon(headUpPixmap);
     ui->headUpButton->setIcon(headUpButtonIcon);
     ui->headUpButton->setIconSize(headUpPixmap.rect().size());
@@ -14,7 +14,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            headUpPixmap.rect().size().height() + 15);
     ui->headUpButton->setFixedSize(headUpButtonSize);
 
-    QPixmap headDownPixmap("images/head-down-icon.jpg");
+    QPixmap headDownPixmap("images/head-down-icon.png");
     QIcon headDownButtonIcon(headDownPixmap);
     ui->headDownButton->setIcon(headDownButtonIcon);
     ui->headDownButton->setIconSize(headDownPixmap.rect().size());
@@ -22,7 +22,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            headDownPixmap.rect().size().height() + 15);
     ui->headDownButton->setFixedSize(headDownButtonSize);
 
-    QPixmap feetUpPixmap("images/feet-up-icon.jpg");
+    QPixmap feetUpPixmap("images/feet-up-icon.png");
     QIcon feetUpButtonIcon(feetUpPixmap);
     ui->feetUpButton->setIcon(feetUpButtonIcon);
     ui->feetUpButton->setIconSize(feetUpPixmap.rect().size());
@@ -30,7 +30,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            feetUpPixmap.rect().size().height() + 15);
     ui->feetUpButton->setFixedSize(feetUpButtonSize);
 
-    QPixmap feetDownPixmap("images/feet-down-icon.jpg");
+    QPixmap feetDownPixmap("images/feet-down-icon.png");
     QIcon feetDownButtonIcon(feetDownPixmap);
     ui->feetDownButton->setIcon(feetDownButtonIcon);
     ui->feetDownButton->setIconSize(feetDownPixmap.rect().size());
@@ -38,7 +38,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            feetDownPixmap.rect().size().height() + 15);
     ui->feetDownButton->setFixedSize(feetDownButtonSize);
 
-    QPixmap trendPixmap("images/trend-icon.jpg");
+    QPixmap trendPixmap("images/trend-icon.png");
     QIcon trendButtonIcon(trendPixmap);
     ui->trendButton->setIcon(trendButtonIcon);
     ui->trendButton->setIconSize(trendPixmap.rect().size());
@@ -46,7 +46,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            trendPixmap.rect().size().height() + 15);
     ui->trendButton->setFixedSize(trendButtonSize);
 
-    QPixmap bedUpPixmap("images/bed-up-icon.jpg");
+    QPixmap bedUpPixmap("images/bed-up-icon.png");
     QIcon bedUpButtonIcon(bedUpPixmap);
     ui->bedUpButton->setIcon(bedUpButtonIcon);
     ui->bedUpButton->setIconSize(bedUpPixmap.rect().size());
@@ -54,7 +54,7 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            bedUpPixmap.rect().size().height() + 15);
     ui->bedUpButton->setFixedSize(bedUpButtonSize);
 
-    QPixmap bedDownPixmap("images/bed-down-icon.jpg");
+    QPixmap bedDownPixmap("images/bed-down-icon.png");
     QIcon bedDownButtonIcon(bedDownPixmap);
     ui->bedDownButton->setIcon(bedDownButtonIcon);
     ui->bedDownButton->setIconSize(bedDownPixmap.rect().size());
@@ -62,14 +62,29 @@ Raspbed::Raspbed(QWidget *parent) : QMainWindow(parent), ui(new Ui::Raspbed), be
                            bedDownPixmap.rect().size().height() + 15);
     ui->bedDownButton->setFixedSize(bedDownButtonSize);
 
-    QPixmap lowerWheelsPixmap("images/lower-wheels-icon.jpg");
+    QPixmap lowerWheelsPixmap("images/lower-wheels-icon.png");
     QIcon lowerWheelsButtonIcon(lowerWheelsPixmap);
     ui->lowerWheelsButton->setIcon(lowerWheelsButtonIcon);
     ui->lowerWheelsButton->setIconSize(lowerWheelsPixmap.rect().size());
     QSize lowerWheelsButtonSize(lowerWheelsPixmap.rect().size().width() + 15,
                            lowerWheelsPixmap.rect().size().height() + 15);
-
     ui->lowerWheelsButton->setFixedSize(lowerWheelsButtonSize);
+
+    QPixmap flattenBedPixmap("images/flatten-icon.png");
+    QIcon flattenBedButtonIcon(flattenBedPixmap);
+    ui->flattenBedButton->setIcon(flattenBedButtonIcon);
+    ui->flattenBedButton->setIconSize(flattenBedPixmap.rect().size());
+    QSize flattenBedButtonSize(flattenBedPixmap.rect().size().width() + 15,
+                           flattenBedPixmap.rect().size().height() + 15);
+    ui->flattenBedButton->setFixedSize(flattenBedButtonSize);
+
+    QPixmap callPixmap("images/call-icon.png");
+    QIcon callButtonIcon(callPixmap);
+    ui->callButton->setIcon(callButtonIcon);
+    ui->callButton->setIconSize(callPixmap.rect().size());
+    QSize callButtonSize(callPixmap.rect().size().width() + 15,
+                           callPixmap.rect().size().height() + 15);
+    ui->callButton->setFixedSize(callButtonSize);
 }
 
 Raspbed::~Raspbed() { delete ui; }
