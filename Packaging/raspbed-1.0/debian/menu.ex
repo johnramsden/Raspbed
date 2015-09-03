634330d8a0e01e49dcdebe61e633d7dd47a253a2
John Ramsden <johnramsden@users.noreply.github.com>
2015-09-02 18:32:18 -0700

    Finished new packaging files

--- Packaging/raspbed-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(raspbed):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="raspbed" command="/usr/bin/raspbed"
--- Packaging/raspbed-1.0/debian/raspbed.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the raspbed package
-#
-0 4	* * *	root	[ -x /usr/bin/raspbed_maintenance ] && /usr/bin/raspbed_maintenance
